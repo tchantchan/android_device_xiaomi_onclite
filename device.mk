f1b00fa120567bc1d5b9bdd37646f9efa1847ec3
bibarub <bibarub@vivaldi.net>
2020-12-08 09:56:40 +0000

    DerpFest bringup

--- device.mk
+++ device.mk
@@ -18,11 +18,11 @@ $(call inherit-product, vendor/xiaomi/onclite/onclite-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-derp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-derp/frameworks
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
